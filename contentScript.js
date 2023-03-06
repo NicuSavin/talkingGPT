084c3b486225c108df5a6fcd3f141022af92b732
Nicu Savin <savinicu@gmail.com>
2023-03-06 19:01:51 +0100

    added sendKeyword. mic and auto button implemented

--- contentScript.js
+++ contentScript.js
@@ -1,3 +1,8 @@
+var spokenLanguage = "en-US";
+var sendKeyword = "submit question";
+
+var autoMic = false;
+
 if (localStorage.getItem("buttonClass") === null) {
   localStorage.setItem(
     "buttonClass",
@@ -21,7 +26,6 @@ function addBtn() {
   if (location !== null && location !== undefined) {
     location = location[0].childNodes[0];
 
-    //creating control panel of the extension, that will have 3 buttons
     let controlPanel = document.createElement("div");
     controlPanel.id = "control_panel";
     controlPanel.style.display = "flex";
@@ -34,9 +38,11 @@ function addBtn() {
     micBtn.id = "mic_btn";
     micBtn.type = "button";
     micBtn.style.width = "33%";
-
     micBtn.onclick = function () {
-      record(location);
+      if (!autoMic) {
+        record();
+        activeMicBtnStyle();
+      }
     };
 
     //button for automatically sending the question after speaking
@@ -46,6 +52,17 @@ function addBtn() {
     autoBtn.id = "auto_btn";
     autoBtn.type = "button";
     autoBtn.style.width = "33%";
+    autoBtn.onclick = function () {
+      autoMic = !autoMic;
+      if (autoMic) {
+        //auto button is activated
+        activeAutoBtnStyle();
+
+        record();
+      } else {
+        resetAutoBtnStyle();
+      }
+    };
 
     //Button for muting chatGPT from speaking(basically make him skip reading)
     let soundBtn = document.createElement("button");
@@ -59,8 +76,6 @@ function addBtn() {
     controlPanel.appendChild(autoBtn);
     controlPanel.appendChild(soundBtn);
 
-    //appending the control panel in the right place
-
     location.appendChild(controlPanel);
   }
 }
@@ -69,4 +84,96 @@ function checkBtn() {
     return false;
   }
   return true;
+}
+
+function activeAutoBtnStyle() {
+  document.getElementById("auto_btn").style.color = "green";
+  document.getElementById("auto_btn").style.border = "1px solid green";
+}
+
+function resetAutoBtnStyle() {
+  document.getElementById("auto_btn").style.color = "";
+  document.getElementById("auto_btn").style.border = "";
+}
+
+function activeMicBtnStyle() {
+  document.getElementById("mic_btn").style.color = "red";
+  document.getElementById("mic_btn").style.border = "1px solid red";
+}
+
+function resetMicBtnStyle() {
+  document.getElementById("mic_btn").style.color = "";
+  document.getElementById("mic_btn").style.border = "";
+}
+
+function record() {
+  speechRecon = new webkitSpeechRecognition();
+  speechRecon.continuous = true;
+  speechRecon.lang = spokenLanguage;
+  speechRecon.start();
+  speechRecon.onstart = () => {
+    activeMicBtnStyle();
+  };
+
+  speechRecon.onresult = (e) => {
+    let msg = "";
+    for (let i = e.resultIndex; i < e.results.length; ++i) {
+      if (e.results[i].isFinal) msg += e.results[i][0].transcript;
+    }
+    speechRecon.stop();
+
+    let sendSplit = sendKeyword.split(" ");
+    let msgSplit = msg.split(" ");
+
+    //find whether the send keyword has been said
+    let send = true;
+    for (k = 0; k < sendSplit.length; k += 1) {
+      if (msgSplit[msgSplit.length - sendSplit.length + k] !== sendSplit[k]) {
+        send = false;
+      }
+    }
+
+    //insert the message in the question box
+    if (document.getElementsByTagName("textarea")[0].value.length !== 0) {
+      if (send) {
+        document.getElementsByTagName("textarea")[0].value =
+          document.getElementsByTagName("textarea")[0].value +
+          " " +
+          msg.slice(0, msg.length - sendKeyword.length);
+      } else {
+        document.getElementsByTagName("textarea")[0].value =
+          document.getElementsByTagName("textarea")[0].value + " " + msg;
+      }
+    } else {
+      if (send) {
+        document.getElementsByTagName("textarea")[0].value = msg.slice(
+          0,
+          msg.length - sendKeyword.length
+        );
+      } else {
+        document.getElementsByTagName("textarea")[0].value = msg;
+      }
+    }
+
+    if (send) {
+      document
+        .getElementsByTagName("form")[0]
+        .childNodes[0].childNodes[1].childNodes[1].click();
+    }
+  };
+
+  speechRecon.onend = () => {
+    if (autoMic) {
+      speechRecon.start();
+    } else {
+      resetMicBtnStyle();
+    }
+  };
+}
+
+function speak(text) {
+  let msg = new SpeechSynthesisUtterance(text);
+  msg.rate = 1;
+  msg.pitch = 1;
+  window.speechSynthesis.speak(msg);
 }
